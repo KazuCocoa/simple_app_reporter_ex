5ed0741e435378c6401bebf8d80d02b653330f36
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2015-06-19 07:47:45 +0900

    add parsed items and its tests

--- lib/reporter/app_store.ex
+++ lib/reporter/app_store.ex
@@ -6,6 +6,45 @@ defmodule Reporter.AppStore do
   defstruct host: Application.get_env(:reporter, :app_host),
             app_id: Application.get_env(:reporter, :app_id)
 
+
+  def parse_to_map(json) do
+    Poison.Parser.parse!(json)
+  end
+
+  def author(json) do
+    Poison.Parser.parse!(json)["feed"]["author"]
+  end
+
+  def entry(json) do
+    Poison.Parser.parse!(json)["feed"]["entry"]
+    |> List.first
+  end
+
+  def icon(json) do
+    Poison.Parser.parse!(json)["feed"]["icon"]
+  end
+
+  def id(json) do
+    Poison.Parser.parse!(json)["feed"]["id"]
+  end
+
+  def link(json) do
+    Poison.Parser.parse!(json)["feed"]["link"]
+  end
+
+  def rights(json) do
+    Poison.Parser.parse!(json)["feed"]["rights"]
+  end
+
+  def title(json) do
+    Poison.Parser.parse!(json)["feed"]["title"]
+  end
+
+  def updated(json) do
+    Poison.Parser.parse!(json)["feed"]["updated"]
+  end
+
+
   @doc ~S"""
   Return URLs with JSON.
 
@@ -37,7 +76,6 @@ defmodule Reporter.AppStore do
   def rss_xml(app_id, locale \\ "en") do
     rss(app_id, locale, "xml")
   end
-
 
   defp rss(app_id, locale, format) do
     Enum.join([
--- test/reporter/app_store_test.exs
+++ test/reporter/app_store_test.exs
@@ -1,4 +1,53 @@
 defmodule Reporter.AppStoreTest do
   use ExUnit.Case, async: true
   doctest Reporter.AppStore
+
+  alias Reporter.AppStore
+
+  setup_all do
+    json = File.read!("./test/data/ios_review.json")
+    {:ok, json: json}
+  end
+
+  test "AppStore.parse_to_map", %{json: json} do
+    assert AppStore.parse_to_map(json) != nil
+  end
+
+  test "AppStore.author", %{json: json} do
+    author = AppStore.author(json)
+    assert author == %{
+      "name" => %{"label" => "iTunes Store"},
+      "uri" => %{"label" => "http://www.apple.com/jp/itunes/"}
+      }
+  end
+
+  test "AppStore.entry", %{json: json} do
+    entry = AppStore.entry(json)
+    assert entry["category"] != nil
+  end
+
+  test "AppStore.icon", %{json: json} do
+    assert AppStore.icon(json) != nil
+  end
+
+  test "AppStore.id", %{json: json} do
+    assert AppStore.id(json) != nil
+  end
+
+  test "AppStore.link", %{json: json} do
+    assert AppStore.link(json) != nil
+  end
+
+  test "AppStore.rights", %{json: json} do
+    assert AppStore.rights(json) != nil
+  end
+
+  test "AppStore.title", %{json: json} do
+    assert AppStore.title(json) != nil
+  end
+
+  test "AppStore.updated", %{json: json} do
+    assert AppStore.updated(json) != nil
+  end
+
 end
