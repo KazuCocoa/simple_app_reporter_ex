02ac991b2ca88b56e191d59561eca745853311e9
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2015-06-21 23:47:41 +0900

    add test cases

--- test/reporter/app_store_test.exs
+++ test/reporter/app_store_test.exs
@@ -6,53 +6,60 @@ defmodule Reporter.AppStoreTest do
 
   setup_all do
     json = File.read!("./test/data/ios_review.json") |> Poison.decode!
-    {:ok, json: json}
+    json_empty = File.read!("./test/data/ios_review_empty.json") |> Poison.decode!
+    {:ok, json: json, empty: json_empty}
   end
 
-  test "AppStore.all_json", %{json: json} do
+  test "AppStore.all_json", %{json: json, empty: json_empty} do
     assert AppStore.all_json(json) != nil
+    assert AppStore.all_json(json_empty) != nil
   end
 
-  test "AppStore.author", %{json: json} do
-    author = AppStore.author(json)
-    assert author == %{
-      "name" => %{"label" => "iTunes Store"},
-      "uri" => %{"label" => "http://www.apple.com/jp/itunes/"}
-      }
+  test "AppStore.author", %{json: json, empty: json_empty} do
+    author = AppStore.author(json) != nil
+    author = AppStore.author(json_empty) != nil
   end
 
-  test "AppStore.entry", %{json: json} do
+  test "AppStore.entry", %{json: json, empty: json_empty} do
     entry = AppStore.entry(json)
     assert Enum.count(entry) == 11
+    assert AppStore.entry(json_empty) == %{}
   end
 
-  test "AppStore.reviews", %{json: json} do
+  test "AppStore.reviews", %{json: json, empty: json_empty} do
     reviews = AppStore.reviews(json)
     assert Enum.count(reviews) == 50
+    assert AppStore.reviews(json_empty) == %{}
   end
 
-  test "AppStore.icon", %{json: json} do
+  test "AppStore.icon", %{json: json, empty: json_empty} do
     assert AppStore.icon(json) != nil
+    assert AppStore.icon(json_empty) != nil
   end
 
-  test "AppStore.id", %{json: json} do
+  test "AppStore.id", %{json: json, empty: json_empty} do
     assert AppStore.id(json) != nil
+    assert AppStore.id(json_empty) != nil
   end
 
-  test "AppStore.link", %{json: json} do
+  test "AppStore.link", %{json: json, empty: json_empty} do
     assert AppStore.link(json) != nil
+    assert AppStore.link(json_empty) != nil
   end
 
-  test "AppStore.rights", %{json: json} do
+  test "AppStore.rights", %{json: json, empty: json_empty} do
     assert AppStore.rights(json) != nil
+    assert AppStore.rights(json_empty) != nil
   end
 
-  test "AppStore.title", %{json: json} do
+  test "AppStore.title", %{json: json, empty: json_empty} do
     assert AppStore.title(json) != nil
+    assert AppStore.title(json_empty) != nil
   end
 
-  test "AppStore.updated", %{json: json} do
+  test "AppStore.updated", %{json: json, empty: json_empty} do
     assert AppStore.updated(json) != nil
+    assert AppStore.updated(json_empty) != nil
   end
 
 end
