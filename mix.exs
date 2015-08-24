4b68cebf83f15381cc22f562b5f95e47df0f76b2
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2015-08-24 23:11:43 +0900

    update poison

--- mix.exs
+++ mix.exs
@@ -27,7 +27,7 @@ defmodule Reporter.Mixfile do
   defp deps do
     [
       {:httpoison, "~> 0.7"},
-      {:poison, "~> 1.4"},
+      {:poison, "~> 1.5"},
       {:quinn, "~> 0.0.3"},
       {:floki, "~> 0.3"},
       {:earmark, "~> 0.1", only: :dev},
